c675c0fc0aff1a281fd5d9be185f334b7952a0ec
Dmitry Kaminskiy <dmitry.kaminskiy@dataart.com>
2022-02-17 20:49:11 +0200

    fix flake8

--- app/currency/views.py
+++ app/currency/views.py
@@ -12,7 +12,6 @@ def generate_password(password_length: int = 10) -> str:
     password = ''
     for _ in range(password_length):
         password += random.choice(chars)
-    print(111)
     return password
 
 
