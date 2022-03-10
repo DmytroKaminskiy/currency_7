79f58ff28099bfadcafdc0b7499f4b44ea6d5c50
Dmytro Kaminskyi <dmytro.kaminskyi92@gmail.com>
2022-03-10 21:21:29 +0200

    lesson7

--- app/currency/views.py
+++ app/currency/views.py
@@ -1,10 +1,43 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 from currency.models import Rate
 
 
 def rate_list(request):
-    rates = []
-    for rate in Rate.objects.all():
-        rates.append([rate.id, rate.sale, rate.buy])
-    return HttpResponse(str(rates))
+    rates = Rate.objects.all()
+    return render(request, 'rate_list.html', context={'rates': rates})
+
+
+def index(request):
+    return render(request, 'index.html')
+
+
+def status_code(request):
+    '''
+    1xx - Info
+    2xx - Success
+      200 - Ok
+      201 - Created
+      202 - Accepted
+      204 - No Content
+    3xx - Redirect
+      301 - Permanent
+      307 - Temporary
+    4xx - Client Error
+      404 - Page Not Found
+      400 - Bad Request
+      401 - Unauthorized
+      403 - Forbidden
+    5xx - Server Error
+      500 - Server fault
+      502 -	Bad Gateway
+      504 - Gateway Timeout
+    '''
+    if request.GET['exit'] == '0':
+        return HttpResponse('OK', status=200)
+        # return HttpResponse('OK', status=301, headers={'Location': 'https://google.com'})
+    elif not request.GET['exit'].isdigit():
+        return HttpResponse('OK', status=400)
+    else:
+        return HttpResponse('Fail')
--- app/settings/urls.py
+++ app/settings/urls.py
@@ -4,5 +4,7 @@ from currency import views as currency_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', currency_views.index),
+    path('sc/', currency_views.status_code),
     path('rate/list/', currency_views.rate_list),
 ]
