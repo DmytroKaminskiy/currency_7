2631b5445808fc88f4acdf56644b9ba6936e4575
DmytroKaminskiy <dmytro.kaminskyi92@gmail.com>
2022-06-02 21:07:27 +0300

    a

--- app/settings/urls.py
+++ app/settings/urls.py
@@ -14,9 +14,10 @@ urlpatterns = [
     path('currency/', include('currency.urls')),
     path('api/v1/', include('api.v1.urls')),
     path('api/v2/', include('api.v2.urls')),
-
-    path('__debug__/', include('debug_toolbar.urls')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns.append(path('__debug__/', include('debug_toolbar.urls')))
